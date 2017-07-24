14b2051a131c05b8e52bf2e9f6443f5a9c5ca692
Summer.Zhou <Summer.Zhou@172-15-10-19.lightspeed.snjsca.sbcglobal.net>
2017-07-24 22:02:58 +0800

    Homework changed
    Ah right

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,17 +2,28 @@
 
 import UIKit
 
-func check(word:String){
-    var result : Bool = true
-    for i in 1..<word.characters.count{
-        let index = word.index(word.startIndex,offsetBy:word.characters.count-i)
-        let finalindex = word.index(word.startIndex,offsetBy:i-1)
-        
-        if (word[index] != word[finalindex]){
-            result = false
+    func check(word:String){
+        var result : Bool = true
+        var interesting : Int = 0
+        for _ in word.characters{
+            interesting = interesting + 1
         }
+
+    
+    
+        for i in 1..<word.characters.count{
+           let index = word.index(word.startIndex,offsetBy:interesting-i)
+           let finalindex = word.index(word.startIndex,offsetBy:i-1)
         
-    }
-    print(result)
-}
+           if (word[index] != word[finalindex]){
+               result = false
+           }
+        
+       }
+        print(result)
+   }
 check(word: "anna")
+
+
+
+
